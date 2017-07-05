63e6afa2c0bc91b632660a7849cff2f57be283d7
Chinmay <chinmay.kulkarni333@gmail.com>
2017-07-06 00:43:46 +0530

    new change

--- src/main/java/com/bank/BankController.java
+++ src/main/java/com/bank/BankController.java
@@ -11,53 +11,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class BankController {
-	
+
 	/*
 	 * 
-	 * REST - Representational State transfer 
-	 * Client - Server architecture
+	 * REST - Representational State transfer Client - Server architecture
 	 * 
 	 * CRUD - Create Read Update Delete
 	 * 
-	 * User - Account 
+	 * User - Account
 	 * 
-	 * HTTP 
+	 * HTTP
 	 * 
-	 * GET - Read
-	 * POST- Create
-	 * PUT - Update
-	 * DELETE - Delete
+	 * GET - Read POST- Create PUT - Update DELETE - Delete
 	 * 
 	 * https://spring.io/guides/gs/rest-service/
-	 * */
-	
+	 */
+
 	@GetMapping
-	public String getUser(@RequestParam (value ="name") String userName, @RequestParam (value="dob") String dob,@RequestParam (value="address") String address,@RequestParam(value="adhar") String adhar) {// Welcome page, non-rest
-		
-		
-		String accountNumner = createAccount(userName, accountNumber);
-		if(!accountNumber.equalsIgnoreCase("kulkarni"))
+	public String getUser(@RequestParam(value = "name") String userName, @RequestParam(value = "dob") String dob,
+			@RequestParam(value = "address") String address, @RequestParam(value = "adhar") String adhar) {// Welcome
+																											// page,
+																											// non-rest
+
+		String accountNumner = createAccount(userName, dob, address, adhar);
+		if (!userName.equalsIgnoreCase("kulkarni"))
 			throw new RuntimeException("You are not Kulkarni");
-		return "Get user executed, with user: "+ userName+" "+accountNumber;
+		return "Get user executed, with user: " + userName + " ";
 	}
-	
-	private String createAccount(String userName, String lastName) {
+
+	private String createAccount(String userName, String lastName, String address, String adhar) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@PostMapping
-	public String createAccount(){
+	public String createAccount() {
 		return "Post account executed";
 	}
-	
+
 	@PutMapping
-	public String updateAccount(){
+	public String updateAccount() {
 		return "Update account Executed";
 	}
-	
+
 	@DeleteMapping
-	public String deleteAccount(){
+	public String deleteAccount() {
 		return "Delete account executed";
 	}
 }
