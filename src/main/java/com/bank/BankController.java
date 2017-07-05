b06355eab9baa8375c8e3ad02d9225719f948fd4
Chinmay <chinmay.kulkarni333@gmail.com>
2017-07-05 21:53:35 +0530

    sts

--- src/main/java/com/bank/BankController.java
+++ src/main/java/com/bank/BankController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,10 +32,20 @@ public class BankController {
 	 * */
 	
 	@GetMapping
-	public String getUser() {// Welcome page, non-rest
-		return "Get user executed";
+	public String getUser(@RequestParam (value ="name") String userName, @RequestParam (value="dob") String dob,@RequestParam (value="address") String address,@RequestParam(value="adhar") String adhar) {// Welcome page, non-rest
+		
+		
+		String accountNumner = createAccount(userName, accountNumber);
+		if(!accountNumber.equalsIgnoreCase("kulkarni"))
+			throw new RuntimeException("You are not Kulkarni");
+		return "Get user executed, with user: "+ userName+" "+accountNumber;
 	}
 	
+	private String createAccount(String userName, String lastName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	@PostMapping
 	public String createAccount(){
 		return "Post account executed";
