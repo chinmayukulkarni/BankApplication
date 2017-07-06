5f1c9627b5361b4f383c05b2f4ab727a710e590f
Chinmay <chinmay.kulkarni333@gmail.com>
2017-07-07 01:40:42 +0530

    via sts code updated

--- src/main/java/com/bank/BankController.java
+++ src/main/java/com/bank/BankController.java
@@ -20,40 +20,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class BankController {
 	ArrayList<Customer> myCustomerArrayList = new ArrayList<Customer>();
 	int customerCounter = 0;
-	Scanner sc1 = new Scanner(System.in);
-	public void readFile_version2() throws IOException {
+	// Scanner sc1 = new Scanner(System.in);
 
-		FileReader myReader = new FileReader("temp3.txt");
-		BufferedReader in = new BufferedReader(myReader);
-
-		String line;
-
-		while ((line = in.readLine()) != null) {
-			// System.out.println(line);
-
-			Customer cust = new Customer();
-			StringTokenizer st = new StringTokenizer(line, "|");
-			while (st.hasMoreTokens()) {
-
-				cust.setCustomerName(st.nextToken());
-				cust.setCustomerAccno(st.nextToken());
-				cust.setCustomerDOB(st.nextToken());
-				cust.setCustomerAddress(st.nextToken());
-				cust.setCustomerPAN(st.nextToken());
-				cust.setCustomerAdhar(st.nextToken().trim());
-				cust.setMoney(st.nextToken().trim());
-				cust.setMobNo(st.nextToken().trim());
-			}
-			myCustomerArrayList.add(cust);
-
-			// myCustomerArraylist.add(customerCounter) = cust;
-			customerCounter++;
-
-		}
-
-		in.close();
-		// System.out.println(customerCounter);
-	}
+	// public void readFile_version2() throws IOException {
+	//
+	// FileReader myReader = new FileReader("temp3.txt");
+	// BufferedReader in = new BufferedReader(myReader);
+	//
+	// String line;
+	//
+	// while ((line = in.readLine()) != null) {
+	// // System.out.println(line);
+	//
+	// Customer cust = new Customer();
+	// StringTokenizer st = new StringTokenizer(line, "|");
+	// while (st.hasMoreTokens()) {
+	//
+	// cust.setCustomerName(st.nextToken());
+	// cust.setCustomerAccno(st.nextToken());
+	// cust.setCustomerDOB(st.nextToken());
+	// cust.setCustomerAddress(st.nextToken());
+	// cust.setCustomerPAN(st.nextToken());
+	// cust.setCustomerAdhar(st.nextToken().trim());
+	// cust.setMoney(st.nextToken().trim());
+	// cust.setMobNo(st.nextToken().trim());
+	// }
+	// myCustomerArrayList.add(cust);
+	//
+	// // myCustomerArraylist.add(customerCounter) = cust;
+	// customerCounter++;
+	//
+	// }
+	//
+	// in.close();
+	// // System.out.println(customerCounter);
+	// }
 	/*
 	 * 
 	 * REST - Representational State transfer Client - Server architecture
@@ -70,16 +71,71 @@ public class BankController {
 	 */
 
 	@GetMapping
-	public String getUser(@RequestParam(value = "name") String name, @RequestParam(value = "dob") String dob,
-			@RequestParam(value = "address") String address, @RequestParam(value = "adhar") String adhar,
-			@RequestParam(value = "money") String money, @RequestParam(value = "mobno") String mobno) {// Welcome
+	public String getUser(@RequestParam(value = "acno") String acno) {// Welcome
 		// page,
 		// non-rest
+		String leftTrimedacno = validateGetUser(acno);
 
-		String accountNumner = createAccount(name, dob, address, adhar, money, mobno);
-		if (!name.equalsIgnoreCase("kulkarni"))
-			throw new RuntimeException("You are not Kulkarni");
-		return "Get user executed, with user: " + name + " ";
+		if (leftTrimedacno == null) {
+			return "The account number is not valid";
+		}
+
+		try {
+			for (int i = 0; i < myCustomerArrayList.size(); i++) {
+				if (myCustomerArrayList.get(i).getCustomerAccno().equals(leftTrimedacno)) {
+
+					return "The Account Number" + leftTrimedacno + " Has Following Data:  \n \nName: "
+							+ myCustomerArrayList.get(i).getCustomerName() + "\nDob: "
+							+ myCustomerArrayList.get(i).getCustomerDOB() + "\nAddress: "
+							+ myCustomerArrayList.get(i).getCustomerAddress() + "\nAdhar: "
+							+ myCustomerArrayList.get(i).getCustomerAdhar() + "\nMoney: "
+							+ myCustomerArrayList.get(i).getMoney() + "\nMobileNumber: "
+							+ myCustomerArrayList.get(i).getMobNo();
+
+				}
+
+			}
+			throw new RuntimeException("Account Not Present in Database or deleted from database");
+
+		} catch (Exception e) {
+			return "error" + e;
+		}
+	}
+
+	private String validateGetUser(String acno) {
+		String rightTrimedacno = acno.trim();
+		String leftTrimedacno = rightTrimedacno.replaceAll("^\\s+", "");
+
+		if (!leftTrimedacno.startsWith("SNGURB")) {
+			return null;
+		}
+
+		// check if the account number is present in database
+		// return the account number if present
+		// return null otherwise
+		String validatedAcno = checkIfAccountNumberIsPresent(leftTrimedacno);
+		return validatedAcno;
+	}
+
+	/**
+	 * This method takes the account number String It checks if the account
+	 * number exists in database or not It also checks if the boolean flag for
+	 * account number is true or not. (It should be true for valid account)
+	 * Returns null otherwise
+	 * 
+	 * @param acno
+	 * @return
+	 */
+	private String checkIfAccountNumberIsPresent(String acno) {
+		for (int i = 0; i < myCustomerArrayList.size(); i++) {
+
+			// Check if the account number is valid and matches with the
+			// customer Number
+			if ((myCustomerArrayList.get(i).isFlag()) && (acno.equals(myCustomerArrayList.get(i).getCustomerAccno()))) {
+				return acno;
+			}
+		}
+		return null;
 	}
 
 	@PostMapping
@@ -103,7 +159,9 @@ public class BankController {
 
 			customerCounter++;
 
-			return customerAccountNumber;
+			return "The Account Number" + customerAccountNumber + " has been created with Following Data:  \nName: "
+					+ name + "\nDob: " + dob + "\nAddress: " + address + "\nAdhar: " + adhar + "\nMoney: " + money
+					+ "\nMobileNumber: " + mobNo + " \n\tSuccessfully";
 
 		} catch (ArithmeticException e) {
 			System.out.println("Warning: ArithmeticException");
@@ -119,37 +177,39 @@ public class BankController {
 
 	@PutMapping
 
-	public Customer updateAccount(@RequestParam(value = "acno") String acno, @RequestParam(value = "name") String name,
-			@RequestParam(value = "dob") String dob, @RequestParam(value = "address") String addes,
+	public String updateAccount(@RequestParam(value = "acno") String acno, @RequestParam(value = "name") String name,
+			@RequestParam(value = "dob") String dob, @RequestParam(value = "address") String address,
 			@RequestParam(value = "adhar") String adhar, @RequestParam(value = "mobNo") String mobNo) {
 		try {
 			for (int i = 0; i < myCustomerArrayList.size(); i++) {
+				if (myCustomerArrayList.get(i).isFlag()
+						&& (acno.equals(myCustomerArrayList.get(i).getCustomerAccno()))) {
+					Customer cust = myCustomerArrayList.get(i);
 
-				Customer cust = myCustomerArrayList.get(i);
+					if (cust.getCustomerAccno().equals(acno)) {
+						cust.setCustomerAccno(acno);
+						cust.setCustomerName(name);
+						cust.setCustomerDOB(dob);
+						cust.setCustomerAddress(address);
+						// cust.setCustomerPAN(pan);
+						cust.setCustomerAdhar(adhar);
+						cust.setMobNo(mobNo);
+						return "The Account Number" + acno + " Has been Updated With following   \nName: " + name
+								+ "\nDob: " + dob + "\nAddress: " + address + "\nAdhar: " + adhar + "\nMobileNumber: "
+								+ mobNo + " \n\tSuccessfully";
 
-				if (cust.getCustomerAccno().equals(acno)) {
-					cust.setCustomerAccno(acno);
-					cust.setCustomerName(name);
-					cust.setCustomerDOB(dob);
-					cust.setCustomerAddress(addes);
-					// cust.setCustomerPAN(pan);
-					cust.setCustomerAdhar(adhar);
-					cust.setMobNo(mobNo);
-					return cust;
-
+					}
 				}
-
 			}
+			return "Account Not Present in Database or deleted from database";
 		} catch (Exception e) {
 
-			System.out.println("Error" + e);
+			return "error in updating account" + e;
 		}
-
-		return null;
 	}
 
 	@DeleteMapping
-	public ArrayList<Customer> deleteAccount(@RequestParam(value = "delcust") String delcust) {
+	public String deleteAccount(@RequestParam(value = "delcust") String delcust) {
 		try {
 			for (int i = 0; i < myCustomerArrayList.size(); i++) {
 				if (myCustomerArrayList.get(i).isFlag()) {
@@ -158,19 +218,17 @@ public class BankController {
 						if (myCustomerArrayList.get(i).getCustomerAccno().equals(delcust)) {
 
 							myCustomerArrayList.get(i).setFlag(false);
-							System.out.println("Account Deleted Successfully");
-							return myCustomerArrayList;
+
+							return "The Account number :- " + delcust + " has been Deleted Successfully";
 						}
 					}
 				}
 			}
-			System.out.println("Account Not Present in Database");
+			return "Account Not Present in Database or deleted from database";
 
 		} catch (Exception e) {
-			System.out.println("Enter Correct Account number");
-
+			return "Enter Correct Account number";
 		}
-		return null;
 	}
 
 }
